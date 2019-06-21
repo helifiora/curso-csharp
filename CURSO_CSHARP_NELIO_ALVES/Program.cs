b262cda0b96ef8bca0001343ec846f21d3f0245f
Helielton Fioramonte <helifiora@gmail.com>
2019-06-21 14:17:14 -0300

    Escopo e Inicialização

--- CURSO_CSHARP_NELIO_ALVES/Program.cs
+++ CURSO_CSHARP_NELIO_ALVES/Program.cs
@@ -7,36 +7,15 @@ namespace CURSO_CSHARP_NELIO_ALVES
     {
         static void Main(string[] args)
         {
-            int x = 10;
+            double preco = double.Parse(Console.ReadLine());
+            double desconto = 0.0;
             
-            Console.WriteLine("Bom dia!");
-            
-            if(x > 5)
-                Console.WriteLine("Boa tarde!");
+            if (preco > 100.0)
+            {
+                desconto = preco * 0.1;
+            }
 
-            Console.WriteLine("Boa noite!");
-
-            Console.WriteLine("-------------------------------------------");
-
-            Console.WriteLine("Entre com o número inteiro: ");
-            int v = int.Parse(Console.ReadLine());
-            
-            if(v % 2 == 0)
-                Console.WriteLine($"Valor {v} é par!");
-            else
-                Console.WriteLine($"Valor {v} é ímpar!");
-
-            Console.WriteLine("--------------------------------------------");
-
-            Console.Write("Qual a Hora Atual? ");
-            int hora = int.Parse(Console.ReadLine());
-            
-            if(hora < 12)
-                Console.WriteLine("Bom Dia!");
-            else if(hora < 18)
-                Console.WriteLine("Boa Tarde!");
-            else
-                Console.WriteLine("Boa Noite!");
+            Console.WriteLine(desconto);
         }
     }
 }

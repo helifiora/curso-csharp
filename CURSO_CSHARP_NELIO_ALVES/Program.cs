9db54eee5c582b42f16dad513790ca7d8ad6fe10
Helielton Fioramonte <helifiora@gmail.com>
2019-06-21 11:36:15 -0300

    1 - Valores Inteiros

--- CURSO_CSHARP_NELIO_ALVES/Program.cs
+++ CURSO_CSHARP_NELIO_ALVES/Program.cs
@@ -6,8 +6,25 @@ namespace CURSO_CSHARP_NELIO_ALVES
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            sbyte x = 100;
+            Console.WriteLine(x);
+
+            SByte y = SByte.MinValue;
+            Console.WriteLine(y);
+
+            byte n2 = 254;
+            n2++;
+            n2++;
+            Console.WriteLine(n2);
+            
+            int n3 = 1000;
+            Console.WriteLine(n3);
+
+            int n4 = Int32.MaxValue;
+            Console.WriteLine(n4);
+
+            long n5 = 2147483648L;
+            Console.WriteLine(n5);
         }
     }
 }

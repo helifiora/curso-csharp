bb0681cd9eae319df528f0a73d7e5a0bc7bbb702
Helielton Fioramonte <helifiora@gmail.com>
2019-06-21 13:55:28 -0300

    Operadores Lógicos

--- CURSO_CSHARP_NELIO_ALVES/Program.cs
+++ CURSO_CSHARP_NELIO_ALVES/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.SymbolStore;
 using System.Globalization;
 
 namespace CURSO_CSHARP_NELIO_ALVES
@@ -7,29 +8,21 @@ namespace CURSO_CSHARP_NELIO_ALVES
     {
         static void Main(string[] args)
         {
-            int a = 10;
-            
-            bool c1 = a < 10;
+            bool c1 = 2 > 3 && 4 != 5;
             Console.WriteLine(c1);
-            
-            bool c2 = a < 20;
+
+            bool c2 = 2 > 3 || 4 != 5;
             Console.WriteLine(c2);
 
-            bool c3 = a > 10;
+            bool c3 = !(2 > 3) && 4 != 5;
             Console.WriteLine(c3);
 
-            bool c4 = a > 5;
+            Console.WriteLine("---------------------------------------");
+
+            bool c4 = 10 < 5;
+            bool c5 = c2 || c3 && c4;
             Console.WriteLine(c4);
-            Console.WriteLine("-----------------------------------");
-            
-            bool c5 = a <= 10;
             Console.WriteLine(c5);
-            bool c6 = a >= 10;
-            Console.WriteLine(c6);
-            bool c7 = a == 10;
-            Console.WriteLine(c7);
-            bool c8 = a != 10;
-            Console.WriteLine(c8);
         }
     }
 }

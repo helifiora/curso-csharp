618da2acb6b0d14e92ce60d0bb3b5d64fba8f743
Helielton Fioramonte <helifiora@gmail.com>
2019-06-21 13:38:44 -0300

    Operadores Comparativos

--- CURSO_CSHARP_NELIO_ALVES/Program.cs
+++ CURSO_CSHARP_NELIO_ALVES/Program.cs
@@ -5,53 +5,31 @@ namespace CURSO_CSHARP_NELIO_ALVES
 {
     class Program
     {
-        static void exercicio()
-        {
-            Console.WriteLine("Entre com seu nome completo:");
-            string nome = Console.ReadLine();
-
-            Console.WriteLine("Quantos quartos tem na sua casa?");
-            byte nQuartos = byte.Parse(Console.ReadLine());
-
-            Console.WriteLine("Entre com o preço de um produto:");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha):");
-            string[] line = Console.ReadLine().Split(' ');
-
-            string ultimoNome = line[0];
-            int idade = int.Parse(line[1]);
-            double altura = double.Parse(line[2], CultureInfo.InvariantCulture);
-
-            Console.WriteLine(nome);
-            Console.WriteLine(nQuartos);
-            Console.WriteLine(preco);
-            Console.WriteLine(ultimoNome);
-            Console.WriteLine(idade);
-            Console.WriteLine(altura);
-        }
         static void Main(string[] args)
         {
-            int n1 = int.Parse(Console.ReadLine());
-            char ch = char.Parse(Console.ReadLine());
-            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            int a = 10;
             
-            string[] line = Console.ReadLine().Split(' ');
-            string nome = line[0];
-            char sexo = char.Parse(line[1]);
-            int idade = int.Parse(line[2]);
-            double altura = double.Parse(line[3], CultureInfo.InvariantCulture);
+            bool c1 = a < 10;
+            Console.WriteLine(c1);
+            
+            bool c2 = a < 20;
+            Console.WriteLine(c2);
 
-            Console.WriteLine("Você digitou: ");
-            Console.WriteLine(n1);
-            Console.WriteLine(ch);
-            Console.WriteLine(n2);
-            Console.WriteLine(nome);
-            Console.WriteLine(sexo);
-            Console.WriteLine(idade);
-            Console.WriteLine(altura);
+            bool c3 = a > 10;
+            Console.WriteLine(c3);
+
+            bool c4 = a > 5;
+            Console.WriteLine(c4);
+            Console.WriteLine("-----------------------------------");
             
-            exercicio();
+            bool c5 = a <= 10;
+            Console.WriteLine(c5);
+            bool c6 = a >= 10;
+            Console.WriteLine(c6);
+            bool c7 = a == 10;
+            Console.WriteLine(c7);
+            bool c8 = a != 10;
+            Console.WriteLine(c8);
         }
     }
 }

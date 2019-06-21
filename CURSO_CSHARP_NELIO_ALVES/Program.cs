b558f5f11a7b51b0dd81a27ce0117c1e3d07b8a1
Helielton Fioramonte <helifiora@gmail.com>
2019-06-21 11:45:42 -0300

    Tipos Basicos

--- CURSO_CSHARP_NELIO_ALVES/Program.cs
+++ CURSO_CSHARP_NELIO_ALVES/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace CURSO_CSHARP_NELIO_ALVES
 {
@@ -6,25 +7,37 @@ namespace CURSO_CSHARP_NELIO_ALVES
     {
         static void Main(string[] args)
         {
-            sbyte x = 100;
-            Console.WriteLine(x);
-
-            SByte y = SByte.MinValue;
-            Console.WriteLine(y);
-
-            byte n2 = 254;
-            n2++;
-            n2++;
-            Console.WriteLine(n2);
-            
-            int n3 = 1000;
-            Console.WriteLine(n3);
-
-            int n4 = Int32.MaxValue;
-            Console.WriteLine(n4);
-
-            long n5 = 2147483648L;
+            bool completo = false;
+            char genero = 'f';
+            char letra = '\u0041';
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            string nome = "Maria Green!";
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
             Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+
+            nome = "John Lennon!";
+            Console.WriteLine(nome);
+
+            object obj1 = "Alex Brown";
+            Console.WriteLine(obj1);
+
+            object obj2 = 4.5f;
+            Console.WriteLine(obj2);
+
+            int nIntMin = int.MinValue;
+            int nIntMax = int.MaxValue;
+            Console.WriteLine(nIntMin + " " + nIntMax);
+
+            sbyte nByteMax = sbyte.MaxValue;
+            Console.WriteLine(nByteMax);
+
+            decimal nDecimal = decimal.MaxValue;
+            Console.WriteLine(nDecimal);
         }
     }
 }

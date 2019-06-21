6906bc2127af659ac7b9ebbcb917a63c610350a0
Helielton Fioramonte <helifiora@gmail.com>
2019-06-21 13:11:12 -0300

    Entrada de Dados 2

--- CURSO_CSHARP_NELIO_ALVES/Program.cs
+++ CURSO_CSHARP_NELIO_ALVES/Program.cs
@@ -5,35 +5,53 @@ namespace CURSO_CSHARP_NELIO_ALVES
 {
     class Program
     {
+        static void exercicio()
+        {
+            Console.WriteLine("Entre com seu nome completo:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            byte nQuartos = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto:");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha):");
+            string[] line = Console.ReadLine().Split(' ');
+
+            string ultimoNome = line[0];
+            int idade = int.Parse(line[1]);
+            double altura = double.Parse(line[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(nQuartos);
+            Console.WriteLine(preco);
+            Console.WriteLine(ultimoNome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura);
+        }
         static void Main(string[] args)
         {
-            Console.Write(">>> ");
-            string frase = Console.ReadLine();
-
-            Console.Write(">>> ");
-            string x = Console.ReadLine();
-
-            Console.Write(">>> ");
-            string y = Console.ReadLine();
-
-            Console.Write(">>> ");
-            string z = Console.ReadLine();
-
-            Console.Write(">>> ");
-            string[] vet = Console.ReadLine().Split(' ');
-            string p1 = vet[0];
-            string p2 = vet[1];
-            string p3 = vet[2];
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             
-            Console.WriteLine("Você digitou:");
-            Console.WriteLine(" " + frase);
-            Console.WriteLine(" " + x);
-            Console.WriteLine(" " + y);
-            Console.WriteLine(" " + z);
-            Console.WriteLine(" " + p1);
-            Console.WriteLine(" " + p2);
-            Console.WriteLine(" " + p3);
+            string[] line = Console.ReadLine().Split(' ');
+            string nome = line[0];
+            char sexo = char.Parse(line[1]);
+            int idade = int.Parse(line[2]);
+            double altura = double.Parse(line[3], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2);
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura);
             
+            exercicio();
         }
     }
 }

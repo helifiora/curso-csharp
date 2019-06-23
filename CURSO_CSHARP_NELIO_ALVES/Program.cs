153967bccfff534c15c01936304c44011fa1b845
Helielton Fioramonte <helifiora@gmail.com>
2019-06-23 11:33:24 -0300

    Problema com OO

--- CURSO_CSHARP_NELIO_ALVES/Program.cs
+++ CURSO_CSHARP_NELIO_ALVES/Program.cs
@@ -3,40 +3,45 @@ using System.Globalization;
 
 namespace CURSO_CSHARP_NELIO_ALVES
 {
+    class Triangulo
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+        
+        
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            double xA, xB, xC;
-            double yA, yB, yC;
+            Triangulo x = new Triangulo();
+            Triangulo y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do Triangulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do Triangulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine($"Area de X: {areaX:F4}");
+            Console.WriteLine($"Area de Y: {areaY:F4}");
             
-            Console.WriteLine("Entre com as medidas do triângulo X: ");
-            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Console.WriteLine("Entra com as medidas do triângulo Y: ");
-            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            double p = (xA + xB + xC) / 2.0;
-            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
-
-            p = (yA + yB + yC) / 2.0;
-            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
-
-            Console.WriteLine($"Área de X: {areaX:F4}");
-            Console.WriteLine($"Área de Y: {areaY:F4}");
-
-            if (areaX > areaY)
-            {
-                Console.WriteLine("Maior Área: X");
-            }
+            if(areaX > areaY)
+                Console.WriteLine("Area maior: X");
             else
-            {
-                Console.WriteLine("Maior Área: Y");
-            }
+                Console.WriteLine("Area menor: Y");
         }
     }
 }

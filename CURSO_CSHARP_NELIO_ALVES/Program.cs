0f8b24c9ade36a94b9b6572c3a17015e71d8f80c
Helielton Fioramonte <helifiora@gmail.com>
2019-06-23 13:15:32 -0300

    Versão 2 - Static

--- CURSO_CSHARP_NELIO_ALVES/Program.cs
+++ CURSO_CSHARP_NELIO_ALVES/Program.cs
@@ -3,26 +3,29 @@ using System.Globalization;
 
 namespace CURSO_CSHARP_NELIO_ALVES
 {
+    class Calculadora
+    {
+        public static double Circunferencia(double raio)
+        {
+            return 2 * Math.PI * raio;
+        }
+
+        public static double Volume(double raio)
+        {
+            return (4.0 / 3.0) * Math.PI * Math.Pow(raio, 3);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.Write("Entre o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double circ = Circunferencia(raio);
-            double volume = Volume(raio);
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
             Console.WriteLine($"Circunferência: {circ:F2}");
-            Console.WriteLine($"Volume: {volume}");
-        }
-
-        static double Circunferencia(double raio)
-        {
-            return 2 * Math.PI * raio;
-        }
-
-        static double Volume(double raio)
-        {
-            return (4.0 / 3.0) * Math.PI * Math.Pow(raio, 3);
+            Console.WriteLine($"Volume: {volume:F2}");
         }
     }
 }

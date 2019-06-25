f331bef7ca88e3bec617538efd465d3ba100dbe8
Helielton Fioramonte <helifiora@gmail.com>
2019-06-24 21:40:35 -0300

    Vetores 2

--- CURSO_CSHARP_NELIO_ALVES/Program.cs
+++ CURSO_CSHARP_NELIO_ALVES/Program.cs
@@ -3,27 +3,39 @@ using System.Globalization;
 
 namespace CURSO_CSHARP_NELIO_ALVES
 {
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Qtd Produtos: ");
             int n = int.Parse(Console.ReadLine());
-            
-            double[] vect = new double[n];
+
+            Product[] products = new Product[n];
             for (int i = 0; i < n; i++)
             {
-                vect[i] = double.Parse(Console.ReadLine());
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                products[i] = new Product()
+                {
+                    Name = name,
+                    Price = price
+                };
             }
 
             double sum = 0.0;
             for (int i = 0; i < n; i++)
             {
-                sum += vect[i];
+                sum += products[i].Price;
             }
 
             double avg = sum / n;
-            Console.WriteLine($"AVERAGE HEIGHT: {avg:F2}");
+            Console.WriteLine($"AVERAGE PRICE: {avg:F2}");
         }
     }
 }

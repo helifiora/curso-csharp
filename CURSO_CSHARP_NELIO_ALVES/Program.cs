c83915bb55dc12015b692ed2346b881b02632a80
Helielton Fioramonte <helifiora@gmail.com>
2019-06-24 21:33:18 -0300

    Vetores 1

--- CURSO_CSHARP_NELIO_ALVES/Program.cs
+++ CURSO_CSHARP_NELIO_ALVES/Program.cs
@@ -7,37 +7,23 @@ namespace CURSO_CSHARP_NELIO_ALVES
     {
         static void Main(string[] args)
         {
-            double? x = null;
-            double? y = 10.0;
-
-            Console.WriteLine(x.GetValueOrDefault());
-            Console.WriteLine(y.GetValueOrDefault());
-            Console.WriteLine();
-            Console.WriteLine(x.HasValue);
-            Console.WriteLine(y.HasValue);
-            Console.WriteLine();
             
-            if(x.HasValue)
-                Console.WriteLine(x.Value);
-            else
-                Console.WriteLine("X is null");
+            int n = int.Parse(Console.ReadLine());
             
-            if(y.HasValue)
-                Console.WriteLine(y.Value);
-            else
-                Console.WriteLine("Y is null");
+            double[] vect = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine());
+            }
 
-            Console.WriteLine();
-            Console.WriteLine();
-            
-            double? a = null;
-            double? b = 10.0;
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += vect[i];
+            }
 
-            double c = a ?? 5.0;
-            double d = b ?? 5.0;
-
-            Console.WriteLine(c);
-            Console.WriteLine(d);
+            double avg = sum / n;
+            Console.WriteLine($"AVERAGE HEIGHT: {avg:F2}");
         }
     }
 }

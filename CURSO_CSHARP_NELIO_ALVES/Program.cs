6beec743f132df06ab6ccbb07895993643cc3e36
Helielton Fioramonte <helifiora@gmail.com>
2019-06-24 21:14:46 -0300

    Tipos Valores

--- CURSO_CSHARP_NELIO_ALVES/Program.cs
+++ CURSO_CSHARP_NELIO_ALVES/Program.cs
@@ -3,85 +3,30 @@ using System.Globalization;
 
 namespace CURSO_CSHARP_NELIO_ALVES
 {
-    class ContaBancaria
+    struct Vetor
     {
-        public int Numero { get; private set; }
-        public string Titular { get; set; }
-        public double Saldo { get; private set; }
+        public double X;
+        public double Y;
 
-        public ContaBancaria(int numero, string titular)
+        public Vetor(double x, double y)
         {
-            Numero = numero;
-            Titular = titular;
+            X = x;
+            Y = y;
         }
-
-        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular)
-        {
-            Deposito(depositoInicial);
-        }
-
-        public void Deposito(double quantia)
-        {
-            Saldo += quantia;
-        }
-
-        public void Saque(double quantia)
-        {
-            Saldo -= quantia;
-            Saldo *= (1 - 0.05);
-        }
-
         public override string ToString()
         {
-            return
-                $"Conta {Numero}, Titular: {Titular}, Saldo: R$ {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+            return $"vetor(x: {X:F2}, y: {Y:F2})";
         }
     }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Entre o número da conta: ");
-            int numero = int.Parse(Console.ReadLine());
-
-            Console.Write("Entre o titular da conta: ");
-            string titular = Console.ReadLine();
-
-            Console.Write("Haverá depósito inicial? ");
-            char resp = char.Parse(Console.ReadLine());
-
-            ContaBancaria conta;
-            if (resp == 's' || resp == 'S')
-            {
-                Console.Write("Entre o valor de depósito inicial: ");
-                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                conta = new ContaBancaria(numero, titular, depositoInicial);
-            }
-            else
-            {
-                conta = new ContaBancaria(numero, titular);
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("Dados da conta");
-            Console.WriteLine(conta);
-
-            Console.WriteLine();
-            Console.Write("Entre um valor para depósito: ");
-            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            conta.Deposito(deposito);
-
-            Console.WriteLine("Dados da conta atualizados: ");
-            Console.WriteLine(conta);
-
-            Console.WriteLine();
-            Console.Write("Entre um valor para saque: ");
-            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            conta.Saque(saque);
-
-            Console.WriteLine("Dados da conta atualizados:");
-            Console.WriteLine(conta);
+            Vetor v = new Vetor(50.0, 60.0);
+            Vetor v2 = v;
+            v2.X = 100;
+            Console.WriteLine(v);
+            Console.WriteLine(v2);
         }
     }
 }

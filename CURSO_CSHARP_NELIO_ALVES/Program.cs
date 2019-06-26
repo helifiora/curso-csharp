5bf476eacf7f316f3c33e8a96e4b8e08d0d318e0
Helielton Fioramonte <helifiora@gmail.com>
2019-06-25 21:36:56 -0300

    List

--- CURSO_CSHARP_NELIO_ALVES/Program.cs
+++ CURSO_CSHARP_NELIO_ALVES/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Collections.Generic;
 
 namespace CURSO_CSHARP_NELIO_ALVES
 {
@@ -7,19 +8,54 @@ namespace CURSO_CSHARP_NELIO_ALVES
     {
         static void Main(string[] args)
         {
-            string[] vetor = new string[] {"Maria", "Alex", "Bob"};
+            List<string> list = new List<string>();
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Anna");
+            list.Insert(2, "Marco");
 
-            for (int i = 0; i < vetor.Length; i++)
-            {
-                Console.WriteLine(vetor[i]);
-            }
-
-            Console.WriteLine("----------------------------------");
-
-            foreach (var nomes in vetor)
+            foreach (var nomes in list)
             {
                 Console.WriteLine(nomes);
             }
+
+            Console.WriteLine("Tamanho: " + list.Count);
+
+            string s1 = list.Find((nome) => nome.StartsWith('A'));
+            Console.WriteLine("Ocorrencia: " + s1);
+
+            string s2 = list.FindLast((nome) => nome.StartsWith('A'));
+            Console.WriteLine("Ultima Ocorrencia: " + s2);
+
+            int pos1 = list.FindIndex((nome) => nome.StartsWith('A'));
+            Console.WriteLine("Primeira Posição: " + pos1);
+
+            int pos2 = list.FindLastIndex((nome) => nome.StartsWith('A'));
+            Console.WriteLine("Ultima Posição: " + pos2);
+
+            List<string> novaLista = list.FindAll((nome) => nome.Length == 5);
+            novaLista.ForEach((nome) => Console.WriteLine(nome));
+
+            Console.WriteLine();
+            list.Remove("Alex");
+            list.ForEach((nome) => Console.WriteLine(nome));
+
+            Console.WriteLine();
+            list.RemoveAll((nome) => nome.StartsWith('M'));
+            list.ForEach((nome) => Console.WriteLine(nome));
+
+            Console.WriteLine();
+            list.RemoveAt(1);
+            list.ForEach((nome) => Console.WriteLine(nome));
+
+            Console.WriteLine();
+            list.Add("Helielton Fioramonte");
+            list.Add("Marina Llase Rossi");
+            list.Add("Carla Chiari");
+            list.Add("Caroline Zuckovski");
+            list.RemoveRange(0, 3);
+            list.ForEach((nome) => Console.WriteLine(nome));
         }
     }
 }

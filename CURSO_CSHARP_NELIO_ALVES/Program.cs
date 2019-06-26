c280eb7751f873b8adff570dd2661113bf92ae9f
Helielton Fioramonte <helifiora@gmail.com>
2019-06-25 21:12:47 -0300

    REF e OUT

--- CURSO_CSHARP_NELIO_ALVES/Program.cs
+++ CURSO_CSHARP_NELIO_ALVES/Program.cs
@@ -13,12 +13,28 @@ namespace CURSO_CSHARP_NELIO_ALVES
             
             return sum;
         }
+
+        public static void Triple(ref int value)
+        {
+            value *= 3;
+        }
+
+        public static void Triple(int valor, out int resultado)
+        {
+            resultado = valor * 3;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine(Calculator.Sum(10, 20, 30, 40, 50, 60, 70, 80, 90, 100));
+            int valor = 5;
+            Calculator.Triple(ref valor); /// REF exige que a variável seja iniciada
+            Console.WriteLine(valor);
+            int resultado;
+            Calculator.Triple(valor, out resultado);
+            Console.WriteLine(resultado);
         }
     }
 }

2809552bbcb12063a52d5154ff7ae3593d908084
Helielton Fioramonte <helifiora@gmail.com>
2019-06-26 21:58:24 -0300

    Matrizes

--- CURSO_CSHARP_NELIO_ALVES/Program.cs
+++ CURSO_CSHARP_NELIO_ALVES/Program.cs
@@ -8,54 +8,36 @@ namespace CURSO_CSHARP_NELIO_ALVES
     {
         static void Main(string[] args)
         {
-            List<string> list = new List<string>();
-            list.Add("Maria");
-            list.Add("Alex");
-            list.Add("Bob");
-            list.Add("Anna");
-            list.Insert(2, "Marco");
-
-            foreach (var nomes in list)
+            int n = int.Parse(Console.ReadLine());
+            
+            int[,] matrix = new int[n, n];
+            for (int i = 0; i < n; i++)
             {
-                Console.WriteLine(nomes);
+                string[] vet = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = int.Parse(vet[j]);
+                }
             }
 
-            Console.WriteLine("Tamanho: " + list.Count);
-
-            string s1 = list.Find((nome) => nome.StartsWith('A'));
-            Console.WriteLine("Ocorrencia: " + s1);
-
-            string s2 = list.FindLast((nome) => nome.StartsWith('A'));
-            Console.WriteLine("Ultima Ocorrencia: " + s2);
-
-            int pos1 = list.FindIndex((nome) => nome.StartsWith('A'));
-            Console.WriteLine("Primeira Posição: " + pos1);
-
-            int pos2 = list.FindLastIndex((nome) => nome.StartsWith('A'));
-            Console.WriteLine("Ultima Posição: " + pos2);
-
-            List<string> novaLista = list.FindAll((nome) => nome.Length == 5);
-            novaLista.ForEach((nome) => Console.WriteLine(nome));
+            Console.WriteLine("Main Diagonal");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(matrix[i, i] + " ");
+            }
 
             Console.WriteLine();
-            list.Remove("Alex");
-            list.ForEach((nome) => Console.WriteLine(nome));
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i, j] < 0)
+                        count++;
+                }
+            }
 
-            Console.WriteLine();
-            list.RemoveAll((nome) => nome.StartsWith('M'));
-            list.ForEach((nome) => Console.WriteLine(nome));
-
-            Console.WriteLine();
-            list.RemoveAt(1);
-            list.ForEach((nome) => Console.WriteLine(nome));
-
-            Console.WriteLine();
-            list.Add("Helielton Fioramonte");
-            list.Add("Marina Llase Rossi");
-            list.Add("Carla Chiari");
-            list.Add("Caroline Zuckovski");
-            list.RemoveRange(0, 3);
-            list.ForEach((nome) => Console.WriteLine(nome));
+            Console.WriteLine("Negative Numbers: " + count);
         }
     }
 }

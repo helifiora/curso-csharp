5e2b6ec93a3cb0bee466fea9deab9c815313a648
Helielton Fioramonte <helifiora@gmail.com>
2019-07-09 13:55:31 -0300

    Estrutura Try-Catch

--- CURSO_CSHARP_NELIO_ALVES/Program.cs
+++ CURSO_CSHARP_NELIO_ALVES/Program.cs
@@ -6,8 +6,22 @@ namespace CURSO_CSHARP_NELIO_ALVES
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division Error: Division by zero is not allowed!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format Error: Input string was not in a correct number");
+            }
         }
     }
 }

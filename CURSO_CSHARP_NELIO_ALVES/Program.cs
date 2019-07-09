b6d705b91c3a3a6869fe9df5701c6e33f5d256f9
Helielton Fioramonte <helifiora@gmail.com>
2019-07-09 13:58:00 -0300

    Finally

--- CURSO_CSHARP_NELIO_ALVES/Program.cs
+++ CURSO_CSHARP_NELIO_ALVES/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CURSO_CSHARP_NELIO_ALVES
 {
@@ -6,21 +7,22 @@ namespace CURSO_CSHARP_NELIO_ALVES
     {
         static void Main(string[] args)
         {
+            FileStream fs = null;
             try
             {
-                int n1 = int.Parse(Console.ReadLine());
-                int n2 = int.Parse(Console.ReadLine());
-
-                int result = n1 / n2;
-                Console.WriteLine(result);
+                fs = new FileStream("data.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
             }
-            catch (DivideByZeroException)
+            catch (FileNotFoundException e)
             {
-                Console.WriteLine("Division Error: Division by zero is not allowed!");
+                Console.WriteLine(e.Message);
             }
-            catch (FormatException)
+            finally
             {
-                Console.WriteLine("Format Error: Input string was not in a correct number");
+                if(fs != null)
+                    fs.Close();
             }
         }
     }
